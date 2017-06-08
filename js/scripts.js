f778def53745ab384a198e333a7c5c9762d5c499
sadiquealijama34 <sadiquealijama34@gmail.com>
2017-06-08 15:03:18 +0300

    completed and tested all the specification

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,27 @@
 //business logic
+var convertNumber = function(num) {
 
+  var decimalNumber = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  var romanNumeral = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+  var romanised = "";
+
+
+  if (num <= 3999) {
+    for (index = 0; index <= decimalNumber.length; index++) {
+      while (decimalNumber[index] <= num) {
+        romanised += romanNumeral[index];
+        num -= decimalNumber[index];
+  }
+};
+  } else {return false;}
+return romanised;
+  }
   //user interface logic
 $(document).ready(function(){
     $("form#roman-1").submit(function(event) {
         event.preventDefault();
           var input = parseInt($("input#roman").val());
-          var result = "";
-          var convertNumber = function(input) {
-
-            var decimalNumber = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
-            var romanNumeral = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
-
-
-
-            for (index = 0; index <= decimalNumber.length; index++) {
-              while (decimalNumber[index] <= input) {
-                result += romanNumeral[index];
-                input -= decimalNumber[index];
-              }
-            }
-          return result;
-            }
-          convertNumber(input);
+          var result = convertNumber(input);
 
           $(".roman").text(result);
 
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Tittle  
+Our Roman Numbers  
+## Author  
+Abubakr Sadique Ali  
+### Description  
+Created a program that converts numbers to roman numerals  
+#### Specifications  
+a) The program allows a user to convert number value to a roman numeral  
+b) The program identifies each symbol with a different value  
+c) The program has a rule that states if symbol I is placed before any other specified symbol  
+  it subtracts and get the result of the two  
+d) The program is able to separate ones, tens, & hundreds  
+e) The program rejects any number that is greater or equal to 4000  
+##### Copyright and Licences  
+&C Sadique Ali/Moringa  
